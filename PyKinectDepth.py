d70f8516928b58bb68fa88559266343699a78b40
Luke <work37@gmail.com>
2018-08-17 19:12:13 +0300

    generating dataset for kinect calibration

--- PyKinectDepth.py
+++ PyKinectDepth.py
@@ -1,3 +1,4 @@
+import threading
 from time import sleep, time
 
 from pykinect2 import PyKinectV2
@@ -16,7 +17,9 @@ fov_horiz = np.deg2rad(70.6)
 fov_vert = np.deg2rad(60)
 fov_horiz_half = fov_horiz / 2
 fov_vert_half = fov_vert / 2
-T = []
+T1 = []
+T2 = []
+T3 = []
 
 
 def find_closest_element_index(frame):
@@ -31,10 +34,10 @@ def find_upper_element_index(frame):
 
 
 def noise_filter(frame, prev1_frame, prev2_frame,prev3_frame):
-    #frame[:10000] = 0
+    frame[:5120] = 0
     frame[-60000:] = 0
     frame[frame < 800] = 0
-    frame[frame > 3000] = 0
+    frame[frame > 4000] = 0
     frame_copy = frame.copy()
 
     if prev3_frame is not None:
@@ -70,37 +73,77 @@ def calc_mean_coords(frame, X, Y, Z, min_ind):
     return X, Y, Z, mean_x, mean_y, mean_z
 
 
-def calc_calibrated_coords_in_mm(x_in_pixels, y_in_pixels, depth):
-    cx = 254.878;
-    cy = 205.395;
-    fx = 365.456;
-    fy = 365.456;
-    k1 = 0.0905474;
-    k2 = -0.26819;
-    k3 = 0.0950862;
-    p1 = 0.0;
-    p2 = 0.0;
+def calc_coords_in_mm(x_in_pixels, y_in_pixels, depth):
+    x_half = x_in_pixels - frame_width_half
+    y_half = -(y_in_pixels - frame_height_half)
+    x_in_mm = depth * np.math.tan(fov_horiz_half) * (x_half / frame_width_half)
+    y_in_mm = depth * np.math.tan(fov_vert_half) * (y_half / frame_height_half)
+    return x_in_mm, y_in_mm, depth
+
+
+def calc_coords_in_mm2(x_in_pixels, y_in_pixels, depth):
+    cx = 254.878
+    cy = 205.395
+    fx = 365.456
+    fy = 365.456
 
     z = depth/1000
-    x = (x_in_pixels - cx) * z / fx;
-    y = (y_in_pixels - cy) * z / fy;
+    x = (x_in_pixels - cx) * z / fx
+    y = (y_in_pixels - cy) * z / fy
     return x, y, z
 
 
-def calc_coords_in_mm(x_in_pixels, y_in_pixels, depth):
-    x_half = x_in_pixels - frame_width_half
-    y_half = y_in_pixels - frame_height_half
-    x_in_mm = depth * np.math.tan(fov_horiz_half) * (x_half / frame_width_half)
-    y_in_mm = depth * np.math.tan(fov_vert_half) * (y_half / frame_height_half)
-    return x_in_mm, y_in_mm, depth
+def calc_coords_in_mm3(x_in_pixels, y_in_pixels, depth):
+    cx = 254.878
+    cy = 205.395
+    fx = 365.456
+    fy = 365.456
+    k1 = 0.0905474
+    k2 = -0.26819
+    k3 = 0.0950862
+    p1 = 0.0
+    p2 = 0.0
+
+    r = x_in_pixels**2 + y_in_pixels**2
+    k = 1 + k1*r + k2*r**2 + k3*r**3
+    x_px_corrected = x_in_pixels*k + 2*p1*x_in_pixels*y_in_pixels + p2*(r+2*x_in_pixels**2)
+    y_px_corrected = y_in_pixels*k + p1*(r+2*y_in_pixels**2) + 2*p2*x_in_pixels*y_in_pixels
+
+    z = depth/1000
+    x = (x_px_corrected - cx) * z / fx
+    y = (y_px_corrected - cy) * z / fy
+    return x, y, z
 
 
-def calc_coords_in_mm(x_in_pixels, y_in_pixels, depth):
-    x_half = x_in_pixels - frame_width_half
-    y_half = y_in_pixels - frame_height_half
-    x_in_mm = depth * np.math.tan(fov_horiz_half) * (x_half / frame_width_half)
-    y_in_mm = depth * np.math.tan(fov_vert_half) * (y_half / frame_height_half)
-    return x_in_mm, y_in_mm, depth
+def test_coordinates(T, points_from_kinect, points_from_robot):
+    print(T, '\n')
+    for i in range(len(points_from_kinect) - 4):
+        coord_from_kinect = points_from_kinect[4 + i]
+        coord_from_kinect = [*coord_from_kinect, 1]
+        coord = T @ np.transpose(coord_from_kinect)
+        real_robot_coords = points_from_robot[4 + i]
+        print(coord[:3].astype(int))
+        print(*real_robot_coords)
+        print('error', np.linalg.norm(coord[:3] - [*real_robot_coords]), '\n')
+
+
+def calc_transform_matrix(points_from_kinect, points_from_robot):
+    a, b, c, d = points_from_kinect[:4]
+    x1, y1, z1 = a[:]
+    x2, y2, z2 = b[:]
+    x3, y3, z3 = c[:]
+    x4, y4, z4 = d[:]
+
+    A, B, C, D = points_from_robot[:4]
+    X1, Y1, Z1 = A[:]
+    X2, Y2, Z2 = B[:]
+    X3, Y3, Z3 = C[:]
+    X4, Y4, Z4 = D[:]
+
+    transformMatrix = get_transform(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4,
+                                    X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, X4, Y4, Z4)
+    T = np.vstack((transformMatrix, [0, 0, 0, 1]))
+    return T
 
 
 class DepthRuntime(object):
@@ -149,8 +192,20 @@ class DepthRuntime(object):
         prev3_frame = None
 
         points_from_robot = []
-        points_from_kinect = []
+        points_from_kinect1 = []
+        points_from_kinect2 = []
+        points_from_kinect3 = []
         dataset = []
+
+        poselist = ([ 0.0248, -0.1748,  1.3079, 0, 0, 0],
+                    [-0.2577,  0.7804,  0.7975, 0, 0, 0],
+                    [ 0.6091,  0.8586,  0.8775, 0, 0, 0],
+                    [ 0.5673, -0.2957,  0.9791, 0, 0, 0],
+
+                    [-0.7596,  0.0925,  1.0222, 0, 0, 0],
+                    [ 0.7372,  0.18387, 1.1031, 0, 0, 0],
+                    [-0.5041,  0.5650,  0.9561, 0, 0, 0],
+                    [-0.1466,  0.1610,  0.6727, 0, 0, 0])
 
 
         # -------- Main Program Loop -----------
@@ -176,63 +231,82 @@ class DepthRuntime(object):
                 # print(mean_x, mean_y, min_ind)
 
                 kx, ky, kz = calc_coords_in_mm(mean_x, mean_y, mean_z)
+                ckx, cky, ckz = calc_coords_in_mm2(mean_x, mean_y, mean_z)
+                cckx, ccky, cckz = calc_coords_in_mm3(mean_x, mean_y, mean_z)
 
-                ckx, cky, ckz = calc_calibrated_coords_in_mm(mean_x, mean_y, mean_z)
-
-                print('my_coords:', kx, ky, kz)
-                print('Alex_coords:', ckx, cky, ckz)
+                #print('my_coords:', kx, ky, kz)
+                #print('Alex_coords:', ckx, cky, ckz)
+                #print('Alex+calibration_coords:', cckx, ccky, cckz)
 
                 # check_user_input an print data
                 if pygame.key.get_pressed()[pygame.K_q]:
-                    points_from_kinect.append((kx, ky, kz))
-                    #pos = robot.get_pos()
+                    points_from_kinect1.append((kx, ky, kz))
+                    points_from_kinect2.append((ckx, cky, ckz))
+                    points_from_kinect3.append((cckx, ccky, cckz))
+                    pos = robot.get_pos()
                     print([kx,ky,kz])
-                    #print([*pos*1000], '\n')
-                    #points_from_robot.append(pos * 1000)
+                    print([*pos*1000], '\n')
+                    points_from_robot.append(pos * 1000)
                     sleep(0.2)
 
-                if pygame.key.get_pressed()[pygame.K_w]:
-                    print(*points_from_kinect)
-                    print(*points_from_robot, '\n')
-                    sleep(0.1)
+                # check_user_input an print data
 
                 if pygame.key.get_pressed()[pygame.K_r]:
-                    coord_from_kinect = [*points_from_kinect[-1], 1]
-                    coord = T @ np.transpose(coord_from_kinect)
+                    def robot_routine():
+                        robot.movej([0, -1.57, 0, 0, 1.57, 0], acc=0.4, vel=0.4)
+                        for pose in poselist:
+                            robot.movex('movej', pose, acc=0.4, vel=0.4)
+                            sleep(5)
+                            points_from_kinect1.append((kx, ky, kz))
+                            points_from_kinect2.append((ckx, cky, ckz))
+                            points_from_kinect3.append((cckx, ccky, cckz))
+                            pos = robot.get_pos()
+                            points_from_robot.append(pos * 1000)
+                            print([int(kx), int(ky), int(kz)])
+                            print([int(ckx*1000), int(cky*1000), int(ckz*1000)])
+                            print([int(cckx), int(ccky), int(cckz)])
+                            print([*pos * 1000], '\n')
+
+                    threading.Thread(target=robot_routine, ).start()
+                    sleep(0.2)
+
+
+                if pygame.key.get_pressed()[pygame.K_w]:
+                    print(*points_from_kinect1)
+                    print(*points_from_kinect2)
+                    print(*points_from_kinect3)
+                    print(*points_from_robot, '\n')
+                    sleep(0.1)
+                '''
+                if pygame.key.get_pressed()[pygame.K_r]:
+                    coord_from_kinect1 = [*points_from_kinect1[-1], 1]
+                    coord_from_kinect2 = [*points_from_kinect2[-1], 1]
+                    coord_from_kinect3 = [*points_from_kinect3[-1], 1]
+                    coord = T1 @ np.transpose(coord_from_kinect1)
+                    coord = T2 @ np.transpose(coord_from_kinect2)
+                    coord = T3 @ np.transpose(coord_from_kinect3)
                     real_robot_coords = points_from_robot[-1]
                     print(coord[:3].astype(int))
                     print(*real_robot_coords)
                     print('error', np.linalg.norm(coord[:3] - [*real_robot_coords]), '\n')
                     pose = [*coord[:3]/1000, 0, 0, 0]
                     print(pose)
-                    #robot.movel(pose, acc=0.2, vel=0.2)
+                    robot.movel(pose, acc=0.2, vel=0.2)
+                '''
 
                 if pygame.key.get_pressed()[pygame.K_t]:
-                    a, b, c, d = points_from_kinect[:4]
-                    x1, y1, z1 = a[:]
-                    x2, y2, z2 = b[:]
-                    x3, y3, z3 = c[:]
-                    x4, y4, z4 = d[:]
-
-                    A, B, C, D = points_from_robot[:4]
-                    X1, Y1, Z1 = A[:]
-                    X2, Y2, Z2 = B[:]
-                    X3, Y3, Z3 = C[:]
-                    X4, Y4, Z4 = D[:]
-
-                    transformMatrix = get_transform(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, X4, Y4, Z4)
-                    T = np.vstack((transformMatrix, [0, 0, 0, 1]))
+                    T1 = calc_transform_matrix(points_from_kinect1, points_from_robot)
+                    T2 = calc_transform_matrix(points_from_kinect2, points_from_robot)
+                    T3 = calc_transform_matrix(points_from_kinect3, points_from_robot)
                     print("TransformMatrix")
-                    print(T, '\n')
+                    print(T1, '\n')
+                    print(T2, '\n')
+                    print(T3, '\n')
 
-                    for i in range(len(points_from_kinect) - 4):
-                        coord_from_kinect = points_from_kinect[4+i]
-                        coord_from_kinect = [*coord_from_kinect,1]
-                        coord = T @ np.transpose(coord_from_kinect)
-                        real_robot_coords = points_from_robot[4+i]
-                        print(coord[:3].astype(int))
-                        print(*real_robot_coords)
-                        print('error', np.linalg.norm(coord[:3]-[*real_robot_coords]), '\n')
+                    test_coordinates(T1, points_from_kinect1, points_from_robot)
+                    test_coordinates(T2, points_from_kinect2, points_from_robot)
+                    test_coordinates(T3, points_from_kinect3, points_from_robot)
+
 
 
                 if pygame.key.get_pressed()[pygame.K_x]:
@@ -266,8 +340,10 @@ class DepthRuntime(object):
         pygame.quit()
 
 
+
+
 __main__ = "Kinect v2 Depth"
-#robot = urx.Robot("10.0.0.2", use_rt=True)
-#robot.set_tcp((0, 0, 0.1, 0, 0, 0))
+robot = urx.Robot("10.0.0.2", use_rt=True)
+robot.set_tcp((0, 0, 0.1, 0, 0, 0))
 game = DepthRuntime();
 game.run();
---
+++ kinect_point_generator.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+class PointGenerator:
+    def _to_robot_point(self, kinect_point, transform_matrix):
+        adjusted_point = np.append(kinect_point, 1)
+        result = np.matmul(transform_matrix, adjusted_point)
+        return result[:-1]
+
+    def generate_kinect_points(self,
+                               min_distance=1.0,
+                               max_distance=2.0,
+                               fov_degrees=(70.6, 60),
+                               distance_slice_count=3,
+                               point_counts_in_slice=(3, 3)):
+        result = []
+        tangents = np.tan(np.deg2rad(fov_degrees) / 2)
+        for z in np.linspace(min_distance, max_distance, distance_slice_count):
+            max_offsets_in_slice = z * tangents
+            # Create points in rows, left to right, top to bottom.
+            for y in np.linspace(max_offsets_in_slice[1], -max_offsets_in_slice[1], point_counts_in_slice[1]):
+                for x in np.linspace(-max_offsets_in_slice[0], max_offsets_in_slice[0], point_counts_in_slice[0]):
+                    new_point = np.array([x, y, z])
+                    result.append(new_point)
+        return np.array(result)
+
+    def find_reachable_points(self,
+                              src_points,
+                              max_radius=1.35,
+                              tool_vertical_offset=0.0, min_height=0, min_cylinder_radius=0.2):
+        result = []
+        for point in src_points:
+            normalized_point = [point[0], point[1], point[2]- tool_vertical_offset]
+            if np.linalg.norm(normalized_point) <= max_radius and \
+                    point[2]> min_height and \
+                    np.linalg.norm(point[:2]) > min_cylinder_radius:
+                result.append(point)
+        return np.array(result)
+
+    def to_robot_points(self,
+                        kinect_points,
+                        transform_matrix=np.identity(4)):
+        result = []
+        for kinect_point in kinect_points:
+            robot_point = self._to_robot_point(kinect_point, transform_matrix)
+            result.append(robot_point)
+        return np.array(result)
+
+    def add_orientations(self, points):
+        # each point is padded with zeros, e.g. [3,2,6] => [3,2,6,0,0,0]
+        result = np.pad(points, ((0, 0), (0, 3)), 'constant')
+        return result
+
+
+transform = np.array([
+    [9.11553612e-02, -2.97533421e-01, -9.00868653e-01, 1.76319118e+00],
+    [-9.99589709e-01, 2.31756532e-02, -1.23105851e-01, 2.17456855e-01],
+    [-8.01612547e-03, 9.80898800e-01, -2.65743949e-01, 9.56882597e-01],
+    [0, 0, 0, 1]
+])
+
+generator = PointGenerator()
+kinect_points = generator.generate_kinect_points(
+    point_counts_in_slice=(10, 10),
+    min_distance=0.8,
+    max_distance=1.8,
+    distance_slice_count=10,
+    fov_degrees=(60, 58))
+
+robot_points = generator.to_robot_points(kinect_points,
+                                         transform_matrix=transform)
+reachable_points = generator.find_reachable_points(
+    robot_points,
+    min_height=0.6,
+    tool_vertical_offset=0.1)
+
+
+final_points = generator.add_orientations(reachable_points)
+
+np.set_printoptions(threshold=np.nan)
+print('Reachable points:\n', np.array2string(final_points, separator=',', max_line_width=100))
+# tcp:(0,0,0.1), radius:1.350
+
